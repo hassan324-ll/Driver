a467b59494aafa2c9075e7f91d55d1c0b201b28f
hassan324-ll <mohammadhassanali2020@gmail.com>
2025-10-22 03:41:54 -0700

    Update login margin and signup logic

--- src/app/components/verify-email/verify-email.ts
+++ src/app/components/verify-email/verify-email.ts
@@ -11,7 +11,7 @@ import { CommonModule } from '@angular/common';
 })
 export class VerifyEmail {
   message: string =
-    'A verification link has been sent to your email. Please verify your email to continue.Check you spam folder.';
+    'A verification link has been sent to your email. Please verify your email to continue.Please check you spam folder.';
   error: string = '';
   loading: boolean = false;
 
